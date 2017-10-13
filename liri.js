5a2d96087a67c251d0b64d3e940988fd59d7df8c
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-10-13 08:55:39 -0700

    creating function to split userinput and interpolate +

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
-// var twitterKeys = require("./keys.js");
-// var spotify = require('node-spotify-api');
+var twitterKeys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 
 // Comands for LIRI
 // my-tweets
@@ -10,20 +10,23 @@
 var request = require("request");
 
 var userRequest = process.argv[2];
-var userInput = process.argv[3];
+var userInput = process.argv;
+var separatedUserInput ="";
 
 switch(userRequest){
   case "my-tweets":
     console.log("twitter");
-    // create twitter function;
+    twitter();
     break;
   case "spotify-this-song":
     console.log("spotify");
-    // create spotify
+    userInputSeparated(userInput);
+    // spotify();
     break;
   case "movie-this":
     console.log("imdb");
-    imdb();
+    userInputSeparated(userInput)
+    imdb(separatedUserInput);
     break;
   case "do-what-it-says":
     console.log("last song");
@@ -31,17 +34,70 @@ switch(userRequest){
     break;
 }
 
-// function twitter(){}
+function twitter(){
+// "https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&count=20"
 
-// function spotify(){}
+// baseUrl = "https://api.twitter.com/1.1/search/tweets.json?q=kanye%20west"
 
-function imdb(){
+twitterKeys.require('search/tweets', {q: 'node.js'}, function(error, response, body){
 
-  var queryurl = "http://www.omdbapi.com/?t="+ userInput + "&apikey=40e9cece"
+  if (!error && response.statusCode === 200) {
+    console.log(response);
+  }
+  else {
+    console.log(error);
+  }
+})
+
+}
+
+function userInputSeparated(searchTerm){
+
+  console.log(searchTerm);
+
+  for ( var i = 3; i < searchTerm.length; i++) {
+
+    if (i > 3 && i < searchTerm.length) {
+      separatedUserInput = separatedUserInput + "+" + searchTerm[i];
+
+    } else {
+      separatedUserInput += searchTerm[i]; 
+    }
+
+  }
+  // console.log("this is the separated user input " + separatedUserInput);
+  return separatedUserInput;
+}
+
+function spotify(){
+
+  var spotify = new Spotify({
+    id: "2183c528246541b1838b6be6e54483cd",
+    secret: "8bfd5409cbe54f24941d130e78b6c6d4"
+  });
+   
+   var spotifyUrl ='https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx'
+
+
+  spotify
+    .request(spotifyUrl +)
+    .then(function(data) {
+      console.log(data); 
+    })
+    .catch(function(err) {
+      console.error('Error occurred: ' + err); 
+    });
+
+function imdb(movieTitle){
+
+  var queryurl = "http://www.omdbapi.com/?t="+ movieTitle + "&apikey=40e9cece"
+
+  console.log("this is the queryUrl " + queryurl);
 
   request(queryurl, function(error, response, body) {
     if (!error && response.statusCode === 200){
       var imdbObj = JSON.parse(body)
+      // console.log(imdbObj);
       var title = imdbObj.Title;
       var year = imdbObj.Year;
       var imdbRating = imdbObj.Ratings[0].Value;
@@ -59,8 +115,8 @@ function imdb(){
       console.log(title + " stars " + actors);
    
     }
-    else{
+    else {
       console.log("didnt work" + error);
     }
   })
-}
+};
