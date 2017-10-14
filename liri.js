998a9a3b5590eea63049847f219d0514285b2425
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-10-14 12:47:37 -0700

    refactored the do-what-it-says to be conditional. Allows user to change text. Man that feels good to work

--- liri.js
+++ liri.js
@@ -21,18 +21,17 @@ var separatedUserInput = "";
 
 switch(userRequest){
   case "my-tweets":
-    console.log("twitter");
-    twitter();
+    renderTweets();
     break;
   case "spotify-this-song":
     // console.log("spotify");
     userInputSeparated(userInput);
-    spotify(separatedUserInput);
+    renderSpotify(separatedUserInput);
     break;
   case "movie-this":
     // console.log("imdb");
     userInputSeparated(userInput)
-    imdb(separatedUserInput);
+    renderImdb(separatedUserInput);
     break;
   case "do-what-it-says":
     console.log("last song");
@@ -43,8 +42,8 @@ switch(userRequest){
 
 // ****FUNCTIONS*******
 
-// finds last 20 tweets for screenname
-function twitter() {
+// render 
+function renderTweets() {
 
   //parms used to capture screen name of account 
   var params = {screen_name: 'CNN'};
@@ -56,14 +55,11 @@ function twitter() {
         console.log(tweets[i].created_at);
         console.log(tweets[i].text);
       }
-      else {
-        throw error;
-      }
     }
   });
 };
 
-// Takes string after command and formats to be separated by "+"
+// Takes input after command and formats to be separated by "+"
 function userInputSeparated(searchTerm) {
 
   // loop through length of input and insert "+"
@@ -81,51 +77,51 @@ function userInputSeparated(searchTerm) {
   return separatedUserInput;
 };
 
-// Takes
-function spotify(songSearch){
-
+// Render artist and song details 
+function renderSpotify(songSearch) {
+  // spotify keys
   var spotify = new Spotify({
     id: "2183c528246541b1838b6be6e54483cd",
     secret: "8bfd5409cbe54f24941d130e78b6c6d4"
   });
-   
-   // var spotifyUrl ='https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx'
+   // If nothing is entered into command line use "The Sign"
    if(songSearch.length == 0) {
     // console.log("works");
     songSearch = "The Sign";
    }
 
+   // When user enters in song title,
   spotify.search({ type: 'track', query: songSearch }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
+
   } else {
   // NEED to do JSON.parse
   var spotifyObj = data;
-  // console.log(spotifyObj);
-  // returns total data
-  // get tracks
+
+  // Gets tracks asssociated with song name
   var spotifyList = spotifyObj.tracks;
-  // console.log(spotifyList);
+
+  //return list for iteration in FOR loop below 
   var lengthOfSpotifyList = spotifyList.items
-  // console.log(lengthOfSpotifyList.length);
-  // console.log(lengthOfSpotifyList)
 
-    for ( var i = 0; i < lengthOfSpotifyList.length; i++) {
-      console.log("")
-      console.log("Artists name is-" + lengthOfSpotifyList[i].artists[0].name);
-      console.log("Song's Name: " + lengthOfSpotifyList[i].name);
-      console.log("The url for the song is "+ lengthOfSpotifyList[i].external_urls.spotify);
-      console.log("The Alubm is: "+ lengthOfSpotifyList[i].album.name)
-      console.log("")
-      console.log("================================")
-    }   
-
-  }
+      // iterate through each item and console in desired format
+      for ( var i = 0; i < lengthOfSpotifyList.length; i++) {
+        console.log("")
+        console.log("Artists name is-" + lengthOfSpotifyList[i].artists[0].name);
+        console.log("Song's Name: " + lengthOfSpotifyList[i].name);
+        console.log("The url for the song is "+ lengthOfSpotifyList[i].external_urls.spotify);
+        console.log("The Alubm is: "+ lengthOfSpotifyList[i].album.name)
+        console.log("")
+        console.log("================================")
+      }   
+    }
   })
 };
 
-function imdb(movieTitle){
-
+// Render movie details
+function renderImdb(movieTitle){
+  // If no movie entered show "Mr Nobody"
   if (movieTitle.length == 0) {
     var queryurl = "http://www.omdbapi.com/?t=mr+nobody&apikey=40e9cece"
 
@@ -133,9 +129,7 @@ function imdb(movieTitle){
   else {
     var queryurl = "http://www.omdbapi.com/?t="+ movieTitle + "&apikey=40e9cece"
   }
-
-    // console.log("this is the queryUrl " + queryurl);
-
+    // print to screen in desired formation
     request(queryurl, function(error, response, body) {
       if (!error && response.statusCode === 200){
         var imdbObj = JSON.parse(body)
@@ -163,6 +157,7 @@ function imdb(movieTitle){
     })
 };
 
+//Take string from random.txt and send through spotify function 
 function readRandomTxt(){
   fs.readFile("random.txt", "utf8", function(error,data){
     if(error) {
@@ -172,14 +167,34 @@ function readRandomTxt(){
     console.log("From the randomtext " + data);
     var splitData = data.split(" ");
     console.log(splitData);
-    splitData.splice(0,1);
-    console.log(splitData)
-    var joinedString = splitData.join();
-    console.log(joinedString);
-    var fromText= joinedString.replace(/,/g , ",");
-    console.log(fromText);
+    var commandArray = splitData.splice(0,1);
+    console.log(commandArray);
+    console.log(splitData);
+
+    var commandtoString = commandArray.toString();
+    console.log(commandtoString);
+
+    if (commandtoString === "my-tweets") {
+      renderTweets();
+    }
+    else if( commandtoString === "spotify-this-song") {
+      var joinedString = splitData.join();
+      console.log(joinedString);
+      var fromText= joinedString.replace(/,/g , ",");
+      console.log(fromText);
  
-    spotify(fromText);
+      renderSpotify(fromText);
+    }
+    else if(commandtoString === "movie-this"){
+      var joinedString = splitData.join();
+      console.log(joinedString);
+      var fromText= joinedString.replace(/,/g , "+");
+      console.log(fromText);
+ 
+      renderImdb(fromText);
+    }
+
+    else {console.log("Broken")}
 
   })
 }
